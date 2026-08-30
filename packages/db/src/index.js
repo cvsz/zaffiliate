efa9b96e22dca6c6181284394c76c5a289dff913
cvsz <seaza@msn.com>
2026-08-30 23:55:34 +0700

    feat(db): export affiliate core repository

--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -1,3 +1,4 @@
 export { createDbClient } from './client.js';
 export { createMigrator, MigrationDriftError } from './migrator.js';
 export { createPublicationJobsRepo, PublicationTransitionError } from './publication-jobs-repo.js';
+export { createAffiliateCoreRepo } from './affiliate-core-repo.js';
